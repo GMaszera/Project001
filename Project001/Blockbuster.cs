793395367324f82b822dc63f0c9791f207ed6a3a
Gabri <Gabri@DESKTOP-135Q8ST>
2023-08-19 23:37:28 -0300

    Adicionar arquivos de projeto.

---
+++ Project001/Blockbuster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project001
+{
+    public class Blockbuster
+    {
+        public string Filme;
+
+        public Blockbuster ( string Filme )
+        {
+            this.Filme = Filme;
+        }
+    }
+}
